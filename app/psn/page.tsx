9118c42a37c5903c632ef3fa2d38ce67ce1e468e
Yicheng Fu <yichengfu@DNa80944c.SUNet>
2025-02-17 21:44:47 -0800

    Add dataset viewer for EgoNormia

---
+++ app/psn/page.tsx
@@ -0,0 +1,9 @@
+import VideoVisualizer from '@/components/VideoVisualizer';
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gray-100 py-8">
+      <VideoVisualizer />
+    </main>
+  );
+}
